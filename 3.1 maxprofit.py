874b3b10ad148721f399406347677e639df188bb
yuvaraj25082004 <122504399+yuvaraj25082004@users.noreply.github.com>
2023-01-12 13:27:16 +0530

    Add files via upload

---
+++ 3.1 maxprofit.py
@@ -0,0 +1,18 @@
+def maxProfit(prices, n, k):
+	profit = [[0 for i in range(k + 1)]
+				for j in range(n)]
+	for i in range(1, n):		
+		for j in range(1, k + 1):
+			max_so_far = 0			
+			for l in range(i):
+				max_so_far = max(max_so_far, prices[i] -
+							prices[l] + profit[l][j - 1])			
+			profit[i][j] = max(profit[i - 1][j], max_so_far)
+	return profit[n - 1][k]
+k = 2
+prices = [2,30,15,10,8,25,80]
+n = len(prices)
+
+print("Maximum profit is:",
+	maxProfit(prices, n, k))
+
